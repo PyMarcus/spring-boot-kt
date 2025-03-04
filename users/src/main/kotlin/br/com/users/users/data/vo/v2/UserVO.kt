9228c39e33b694fc1f70d1a7f4a2af103ae62842
PyMarcus <marcus-v@outlook.com>
2025-03-04 19:25:20 -0300

    feat: Added versions

---
+++ users/src/main/kotlin/br/com/users/users/data/vo/v2/UserVO.kt
@@ -0,0 +1,6 @@
+package br.com.users.users.data.vo.v1
+
+class UserVO (
+    var id: Long = 0,
+    var name: String = ""
+)
---
+++ users/src/main/resources/db/migration/V2__Populate_Table_User.sql
@@ -0,0 +1,5 @@
+INSERT INTO `users` (`id`, `name`) VALUES
+	(1, 'jose da silva quântico'),
+	(2, 'maria da silva quântica'),
+	(3, 'mané'),
+	(4, 'mané2')
